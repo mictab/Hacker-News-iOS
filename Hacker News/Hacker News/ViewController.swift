5250c98059098e942b2eb47a5d32cb051b9e332a
Michel Tabari <michel@lifesum.com>
2016-05-02 11:59:50 +0200

    Initial setup

---
+++ Hacker News/Hacker News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hacker News
+//
+//  Created by Michel Tabari on 5/2/16.
+//  Copyright © 2016 Michel Tabari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Hacker News/Hacker NewsTests/Hacker_NewsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hacker_NewsTests.swift
+//  Hacker NewsTests
+//
+//  Created by Michel Tabari on 5/2/16.
+//  Copyright © 2016 Michel Tabari. All rights reserved.
+//
+
+import XCTest
+@testable import Hacker_News
+
+class Hacker_NewsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
