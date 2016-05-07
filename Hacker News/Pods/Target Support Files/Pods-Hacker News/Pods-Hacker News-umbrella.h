c3a8b357bee389e378167aebe60f489a628db4bc
Michel Tabari <michel@lifesum.com>
2016-05-07 13:47:41 +0200

    Add Alamofire and SwiftyJSON to Podfile

---
+++ Hacker News/Pods/Target Support Files/Pods-Hacker News/Pods-Hacker News-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Hacker_NewsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Hacker_NewsVersionString[];
+
---
+++ Hacker News/Pods/Target Support Files/Pods-Hacker NewsTests/Pods-Hacker NewsTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Hacker_NewsTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Hacker_NewsTestsVersionString[];
+
